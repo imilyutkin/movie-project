f25dcaa04a848da3de06a59747f9516edb6c1b5d
Ivan Milyutkin <ivan.milyutkin@gmail.com>
2015-04-21 16:17:56 +0300

    added simple service

--- app/scripts/controllers/popularmoviescontroller.js
+++ app/scripts/controllers/popularmoviescontroller.js
@@ -8,10 +8,12 @@
  * Controller of the movieProjectApp
  */
 angular.module('movieProjectApp')
-  .controller('PopularMoviesCtrl', function ($scope) {
-    $scope.movies = [
-        {
-            "name": "popular"
-        }
-    ];
+  .controller('PopularMoviesCtrl', function ($scope, $http) {
+    var url = "https://api.themoviedb.org/3/movie/popular?api_key=8ca72ae90f15b5d823a990ab310a5160";
+     $http.get(url).success(function(data, status, headers, config) {
+            $scope.movies = data.results;
+          }).
+          error(function(data, status, headers, config) {
+            $scope.movies =  null;
+          });
   });
--- app/scripts/services/movieservice.js
+++ app/scripts/services/movieservice.js
@@ -8,6 +8,18 @@
  * Service in the movieProjectApp.
  */
 angular.module('movieProjectApp')
-  .service('MovieService', function () {
-    // AngularJS will instantiate a singleton by calling "new" on this function
+  .service('MovieService', function ($http) {
+    function getPopularMovies(){
+        var url = getUrlForRequest(popular);
+        $http.get(url).success(function(data, status, headers, config) {
+            return data.results;
+          }).
+          error(function(data, status, headers, config) {
+            return null;
+          });
+    }
+
+    function getUrlForRequest(type) {
+        return "https://api.themoviedb.org/3/movie/popular?api_key=8ca72ae90f15b5d823a990ab310a5160";
+    }
   });
